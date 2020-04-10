94c8910f54dfe9310a91b58db65ec3940ac8bcaf
John Chen <john.person1@gmail.com>
2020-04-10 14:11:02 -0400

    get minimal working example working

--- csc413_pa1_q23_backtest.js
+++ csc413_pa1_q23_backtest.js
@@ -12,15 +12,8 @@
 function enter_marks(){
 
     console.log("script running");
-    const top_level = document.querySelector("#rubric_criterion_8")
-    console.log(top_level)
-    top_level.click()
-    console.log("Question clicked!")
     const set_mark = document.querySelector("#rubric_criterion_8 > div > table > tbody > tr:nth-child(5)").click()
     console.log("Mark set!")
-
-    //    document.querySelector("#rubric_criterion_8 > div > div").click()
-  //  document.querySelector("#rubric_criterion_8 > div > table > tbody > tr:nth-child(5) > td.level-description").click()
     console.log("finished execution");
 }
 
