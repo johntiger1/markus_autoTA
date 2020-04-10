0e1f24343281040671cbbeeebb80276b4581e77d
John Chen <john.person1@gmail.com>
2020-04-10 14:09:36 -0400

    clicking with a timeout of 2k ms works (guess it takes a while for the react comp to render). we dont even need to click on the dropdown to expand it

--- csc413_pa1_q23_backtest.js
+++ csc413_pa1_q23_backtest.js
@@ -1,19 +1,16 @@
 // ==UserScript==
-// @name         CSC413 Q2/Q3 autoscript
+// @name         CSC413 HW4 Q1 autoscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Marking automation
 // @author       John
 // @match https://markus.teach.cs.toronto.edu/*
 // @grant        none
-// @require https://unpkg.com/browse/react-dom@15.6.1/lib/ReactTestUtils.js
 // ==/UserScript==
 
-(function() {
-    //'use strict';
-    //var ReactTestUtils = require('react-dom/test-utils')
-    //var ReactTestUtils = window.
-    console.log(ReactTestUtils)
+
+function enter_marks(){
+
     console.log("script running");
     const top_level = document.querySelector("#rubric_criterion_8")
     console.log(top_level)
@@ -25,14 +22,6 @@
     //    document.querySelector("#rubric_criterion_8 > div > div").click()
   //  document.querySelector("#rubric_criterion_8 > div > table > tbody > tr:nth-child(5) > td.level-description").click()
     console.log("finished execution");
-// @include http://*/*
-// @include https://*/*
-    // @include http://*/*
-// @include https://markus.teach.cs.toronto.edu/csc413-2020-01/assignments/2/*/*
-    // Your code here...
-    // @match https://markus.teach.cs.toronto.edu/csc413-2020-01/assignments/2/*/*
-    // @include https://markus.teach.cs.toronto.edu/csc413-2020-01/assignments/2/*/*
-// @include http://markus.teach.cs.toronto.edu/csc413-2020-01/assignments/2/*/*
-// @include http://*/*
-// @include https://*/*
-})();
+}
+
+setTimeout(enter_marks,2000);
